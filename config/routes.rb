0250b49e9216f5dd91be61348df8bf89c58aa0f6
uimarshall <uimarshall@gmail.com>
2020-09-03 11:29:21 +0100

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #This means a user points its request to d "pages" controller and calls the "home" mtd
   #The controller then picks the corresponding 'home page in the views' and sends to d user
-  root to: "pages#home" 
+  root to: "pages#home"
+  get 'about', to: 'pages#about' 
 end
