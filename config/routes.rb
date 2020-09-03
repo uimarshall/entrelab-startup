fc045bf1848adc9388226ffbb4b1deb38da6c401
uimarshall <uimarshall@gmail.com>
2020-09-03 10:17:21 +0100

    Add custom google fonts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
+  #This means a user points its request to d "pages" controller and calls the "home" mtd
+  #The controller then picks the corresponding 'home page in the views' and sends to d user
+  root to: "pages#home" 
 end
