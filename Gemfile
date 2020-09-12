647dc90a7478081d49663cf3fdb3c156816ce35a
uimarshall <uimarshall@gmail.com>
2020-09-12 11:08:55 +0100

    Adds about page bootstrap gem set up.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby '2.6.6'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
+
+#Use bootstrap for frontend-UI layout
+
+# gem 'bootstrap', '~> 4.5', '>= 4.5.2'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3', '~> 1.4'
 group :development, :test do
@@ -17,6 +21,7 @@ end
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
+gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
@@ -44,6 +49,7 @@ group :development do
   gem 'rubocop', '~>0.81.0'
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 3.3.0'
+  gem 'hirb', '~> 0.7.3'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
+# THE MODUS OPERANDI
+# ******************************************************************************************
+# 1. A req is made to a route e.g 'http://localhost:3000/about'
+# The router then looks for the pages folder('pages#about') and locate the 'PagesController'
+# Inside the controller it looks for the 'about' method and calls it.
+# The 'about' mtd then looks for the corresponding views(about.html.erb) inside the 'views folder'
+# The 'about.html.erb' is then injection into 'application.html.erb' via '<%= yield %>' in the body
+# **************************************************************************************************
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #This means a user points its request to d "pages" controller and calls the "home" mtd
   #The controller then picks the corresponding 'home page in the views' and sends to d user
   root to: "pages#home"
-  get 'about', to: 'pages#about' 
+  get 'about', to: 'pages#about' #A 'get' req points to '/about' and calls about mtd in pages controller
 end
