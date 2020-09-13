9866bf813a97151bc69ffe0266320374172ad524
uimarshall <uimarshall@gmail.com>
2020-09-13 19:46:19 +0100

    modifies contact model file

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
+    # Any object you want your applicatn to work with is stored here
+    # e.g A User object whenever a user signs up will contain an array of objects
+    # The model file is the blue print for our objects,used in creating objects
     
 end
