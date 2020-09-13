79e843e896975ead583eef07437f6a94324e6b45
uimarshall <uimarshall@gmail.com>
2020-09-13 21:40:50 +0100

    Adds Create action for adding data to the database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,22 @@
 class ContactsController < ApplicationController
-  def new; end
+  def new
+    @contact = Contact.new # An instance of the Contact class in the model
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: 'Contact information saved!'
+    else
+      redirect_to new_contact_path, notice: 'An error occured, try again!'
+
+    end
+  end
+  #   Whitelist info collected fromm the forms -- meaning ensure info is securely saved to Db
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :comments)
+  end
 end
