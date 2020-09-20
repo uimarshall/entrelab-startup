544b9f77e0700e10bf77255580fff11385ef9074
uimarshall <uimarshall@gmail.com>
2020-09-20 14:35:05 +0100

    Add association for plan and users
    A user can only belongs_to a basic or pro plan
    A Plan has many users

---
+++ app/models/plan.rb
@@ -0,0 +1,8 @@
+class Plan < ActiveRecord::Base
+    has_many :users
+
+    
+
+    private
+        
+end
---
+++ db/migrate/20200920132639_add_plan_to_user.rb
@@ -0,0 +1,6 @@
+class AddPlanToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :plan_id, :integer
+  
+  end
+end
