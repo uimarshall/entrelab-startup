6dc787845971b133d88959c2a9013fd868739f8f
uimarshall <uimarshall@gmail.com>
2020-09-22 22:17:08 +0100

    Add server side code for stripe user subscriprtion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Whitelist the form fields
+  protected
+
+  def configure_permitted_parameters
+    # rubocop:disable  Layout/LineLength
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
+    # rubocop:enable  Layout/LineLength
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Plan.create(name: 'basic', price: 0)
 Plan.create(name: 'pro', price: 10)
+
