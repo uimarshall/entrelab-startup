9fbebd7edce8e130b7c989f5184c8a83ea5c5089
uimarshall <uimarshall@gmail.com>
2020-09-24 19:46:16 +0100

    Add user profile form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,10 @@
 class ProfilesController < ApplicationController
     # GET to /users/:user_id/profile/new
-
+ 
     def new
         # Render blank profile details form
+        # We can initantialize instance variables in our controller and make it available in the views
+        @profile = Profile.new
         
     end
 end
