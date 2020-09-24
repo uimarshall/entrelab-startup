02293f13ca4175a79badcb2db3c9f8fae22c5e46
uimarshall <uimarshall@gmail.com>
2020-09-24 21:37:21 +0100

    Add create action for user profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,33 @@
 class ProfilesController < ApplicationController
     # GET to /users/:user_id/profile/new
- 
     def new
         # Render blank profile details form
         # We can initantialize instance variables in our controller and make it available in the views
         @profile = Profile.new
         
     end
+    # POST   /users/:user_id/profile
+    def create
+        # Ensure we have the user filling out the form
+        @user = User.find(params[:user_id])
+        # create profile linked to this specific user
+        @profile = @user.build_profile(profile_params)
+        if @profile.save
+            flash[:success] = 'Profile Successfully created'
+            redirect_to root_path
+        else 
+            render action: :new   
+            
+        end
+        # Only this whitelisted fields can go to out db
+       
+        
+    end
+     private
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+            
+            
+        end
+    
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,4 @@
+# The Profile model programmatically has access to all the fields in the profile schema
 class Profile < ActiveRecord::Base
     belongs_to :user
     
