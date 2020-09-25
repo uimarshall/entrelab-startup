b71e46500e733479d838d8d3f58563a649c4e87d
uimarshall <uimarshall@gmail.com>
2020-09-25 15:24:20 +0100

    Add helper for job title icon

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,35 +1,33 @@
 class ProfilesController < ApplicationController
-    # GET to /users/:user_id/profile/new
-    def new
-        # Render blank profile details form
-        # We can initantialize instance variables in our controller and make it available in the views
-        @profile = Profile.new
-        
+  # GET to /users/:user_id/profile/new
+  def new
+    # Render blank profile details form
+    # We can initantialize instance variables in our controller and make it available in the views
+    @profile = Profile.new
+  end
+
+  # POST   /users/:user_id/profile
+  def create
+    # Ensure we have the user filling out the form
+    @user = User.find(params[:user_id])
+    # create profile associated with this specific user
+    # we use '@user.build_profile' bcos a user 'has_one' profile
+    # we will use '@user.profiles.build' for a user 'has_many' profiles
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = 'Profile Successfully created'
+      redirect_to user_path(params[:user_id]) # we need to pass in an 'id' bcos of the URI '/users/:id'
+    else
+      render action: :new
+
     end
-    # POST   /users/:user_id/profile
-    def create
-        # Ensure we have the user filling out the form
-        @user = User.find(params[:user_id])
-        # create profile associated with this specific user
-        # we use '@user.build_profile' bcos a user 'has_one' profile
-        # we will use '@user.profiles.build' for a user 'has_many' profiles
-        @profile = @user.build_profile(profile_params)
-        if @profile.save
-            flash[:success] = 'Profile Successfully created'
-            redirect_to user_path(params[:user_id])#we need to pass in an 'id' bcos of the URI '/users/:id'
-        else 
-            render action: :new   
-            
-        end
-        # Only this whitelisted fields can go to out db
-       
-        
-    end
-     private
-        def profile_params
-            params.require(:profile).permit(:first_name, :last_name,:avatar, :job_title, :phone_number, :contact_email, :description)
-            
-            
-        end
-    
+    # Only this whitelisted fields can go to out db
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile)
+      .permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 # The Profile model programmatically has access to all the fields in the profile schema
 class Profile < ActiveRecord::Base
-    belongs_to :user
-     has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
+  belongs_to :user
+  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' },
+                             default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage/.*\z}
 end
